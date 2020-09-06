07f71afa9f1503e11db93183177ce58c9a98443b
Paul Rojas <paul@Pauls-MacBook-Pro.local>
2020-09-06 17:19:33 -0400

    completed

--- create.php
+++ create.php
@@ -14,5 +14,35 @@ if (!empty($_POST)) {
     $title = isset($_POST['title']) ? $_POST['title'] : '';
     $created = isset($_POST['created']) ? $_POST['created'] : date('Y-m-d H:i:s');
     // Insert new record into the contacts table 
-    
+    $stmt = $pdo->prepare('INSERT INTO contacts VALUES (?,?,?,?,?,?)');
+    $stmt->execute([$id, $name, $email, $phone, $title, $created]);
+    // Output message
+    $msg = 'Created Successfully!';
 }
+?>
+
+<?= template_header('Create') ?>
+
+<div class="content update">
+    <h2>Create Contact</h2>
+    <form action="create.php" method="post">
+        <label for="id">ID</label>
+        <label for="name">Name</label>
+        <input type="text" name="id" placeholder="26" value="auto" id="id">
+        <input type="text" name="name" placeholder="John Doe" id="name">
+        <label for="email">Email</label>
+        <label for="phone">Phone</label>
+        <input type="text" name="email" placeholder="johndoe@example.com" id="email">
+        <input type="text" name="phone" placeholder="2025550143" id="phone">
+        <label for="title">Title</label>
+        <label for="created">Created</label>
+        <input type="text" name="title" placeholder="Employee" id="title">
+        <input type="datetime-local" name="creaed" value="<?= date('Y-m-d\TH:i') ?>" id="created">
+        <input type="submit" value="Create">
+    </form>
+    <?php if ($msg) : ?>
+    <p><?= $msg ?></p>
+    <?php endif; ?>
+</div>
+
+<?= template_footer() ?>
